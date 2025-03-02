0898889751e0ef7b11b44a59ed672d4dffe06d0c
athompson-tamu <thompson@physics.tamu.edu>
2025-03-02 10:07:34 -0600

    renaming to non generic

---
+++ flux_config.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+
+
+# Load fluxes: energy spectra
+numu_flux_500GeV = np.genfromtxt("data/numu_energy_flux_MeV_SqrtS-500GeV_1e19nu.txt")
+nue_flux_500GeV = np.genfromtxt("data/nuebar_energy_flux_MeV_SqrtS-500GeV_1e19nu.txt")
+
+numu_flux_3TeV = np.genfromtxt("data/numu_energy_flux_MeV_SqrtS-3TeV_1e19nu.txt")
+nue_flux_3TeV = np.genfromtxt("data/nuebar_energy_flux_MeV_SqrtS-3TeV_1e19nu.txt")
+
+numu_flux_10TeV = np.genfromtxt("data/numu_energy_flux_MeV_SqrtS-10TeV_1e19nu.txt")
+nue_flux_10TeV = np.genfromtxt("data/nuebar_energy_flux_MeV_SqrtS-10TeV_1e19nu.txt")
+
+
+
+
+# Averaged ssw values and q values
+fluxes_dict = {
+    "500GeV": {
+        "numu_flux": numu_flux_500GeV,
+        "nue_flux": nue_flux_500GeV,
+        "Emu": 250.0,
+        "SSW_MUM": 0.23749462059310783,
+        "Q_MUM": 0.18954159183676164,
+        "SSW_MUP": 0.23743605166405937,
+        "Q_MUP": 0.21873393526869891
+    },
+    "3TeV": {
+        "numu_flux": numu_flux_3TeV,
+        "nue_flux": nue_flux_3TeV,
+        "Emu": 1500.0,
+        "SSW_MUM": 0.2370024038686747,
+        "Q_MUM": 0.46397084998088256,
+        "SSW_MUP": 0.23690402177488004,
+        "Q_MUP": 0.5360353819918382
+    },
+    "10TeV": {
+        "numu_flux": numu_flux_10TeV,
+        "nue_flux": nue_flux_10TeV,
+        "Emu": 5000.0,
+        "SSW_MUM": 0.2365412660420519,
+        "Q_MUM": 0.8462992606999028,
+        "SSW_MUP": 0.23641179871496684,
+        "Q_MUP": 0.9781049328857653
+    }
+}
+
+
+
+
+# Q bins to guarantee enough stats (GeV)
+q_bins_500 = np.logspace(np.log10(0.005), np.log10(0.55), 25)
+q_centers_500 = (q_bins_500[1:] + q_bins_500[:-1])/2
+
+q_bins_3TeV = np.logspace(np.log10(0.01), np.log10(1.4), 25)
+q_centers_3TeV = (q_bins_3TeV[1:] + q_bins_3TeV[:-1])/2
+
+q_bins_10TeV = np.logspace(-1.69, 0.34, 26)
+q_centers_10TeV = (q_bins_10TeV[1:] + q_bins_10TeV[:-1])/2
