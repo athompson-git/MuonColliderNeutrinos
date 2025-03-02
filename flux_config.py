60866d7fdb3457561249f731e94c6e0d3465078b
athompson-tamu <thompson@physics.tamu.edu>
2025-03-02 10:20:00 -0600

    combining bins into dict

--- flux_config.py
+++ flux_config.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from muon_collider_mc import *
 
 
 
@@ -14,6 +14,18 @@ numu_flux_10TeV = np.genfromtxt("data/numu_energy_flux_MeV_SqrtS-10TeV_1e19nu.tx
 nue_flux_10TeV = np.genfromtxt("data/nuebar_energy_flux_MeV_SqrtS-10TeV_1e19nu.txt")
 
 
+# Q bins to guarantee enough stats (GeV)
+q_bins_500 = np.logspace(np.log10(0.005), np.log10(0.55), 25)
+er_bins_MeV_500 = (q_bins_500)**2 / (2*M_E)
+q_centers_500 = (q_bins_500[1:] + q_bins_500[:-1])/2
+
+q_bins_3TeV = np.logspace(np.log10(0.01), np.log10(1.4), 25)
+er_bins_MeV_3TeV = (1e3*q_bins_3TeV)**2 / (2*M_E)
+q_centers_3TeV = (q_bins_3TeV[1:] + q_bins_3TeV[:-1])/2
+
+q_bins_10TeV = np.logspace(-1.69, 0.34, 26)
+er_bins_MeV_10TeV = (q_bins_10TeV)**2 / (2*M_E)
+q_centers_10TeV = (q_bins_10TeV[1:] + q_bins_10TeV[:-1])/2
 
 
 # Averaged ssw values and q values
@@ -25,7 +37,8 @@ fluxes_dict = {
         "SSW_MUM": 0.23749462059310783,
         "Q_MUM": 0.18954159183676164,
         "SSW_MUP": 0.23743605166405937,
-        "Q_MUP": 0.21873393526869891
+        "Q_MUP": 0.21873393526869891,
+        "er_bins_MeV": er_bins_MeV_500
     },
     "3TeV": {
         "numu_flux": numu_flux_3TeV,
@@ -34,7 +47,8 @@ fluxes_dict = {
         "SSW_MUM": 0.2370024038686747,
         "Q_MUM": 0.46397084998088256,
         "SSW_MUP": 0.23690402177488004,
-        "Q_MUP": 0.5360353819918382
+        "Q_MUP": 0.5360353819918382,
+        "er_bins_MeV": er_bins_MeV_3TeV
     },
     "10TeV": {
         "numu_flux": numu_flux_10TeV,
@@ -43,19 +57,7 @@ fluxes_dict = {
         "SSW_MUM": 0.2365412660420519,
         "Q_MUM": 0.8462992606999028,
         "SSW_MUP": 0.23641179871496684,
-        "Q_MUP": 0.9781049328857653
+        "Q_MUP": 0.9781049328857653,
+        "er_bins_MeV": er_bins_MeV_10TeV
     }
 }
-
-
-
-
-# Q bins to guarantee enough stats (GeV)
-q_bins_500 = np.logspace(np.log10(0.005), np.log10(0.55), 25)
-q_centers_500 = (q_bins_500[1:] + q_bins_500[:-1])/2
-
-q_bins_3TeV = np.logspace(np.log10(0.01), np.log10(1.4), 25)
-q_centers_3TeV = (q_bins_3TeV[1:] + q_bins_3TeV[:-1])/2
-
-q_bins_10TeV = np.logspace(-1.69, 0.34, 26)
-q_centers_10TeV = (q_bins_10TeV[1:] + q_bins_10TeV[:-1])/2
