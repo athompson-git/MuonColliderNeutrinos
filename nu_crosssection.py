e36ef616b889238a882ce92652198ac213b21e13
athompson-git <a.thompson@northwestern.edu>
2025-09-02 14:21:57 -0500

    adding nonfactorizable terms with real component of polylogs, vectorized for input arrays and mpmath to float conversion. Still working out bugs with logs

--- nu_crosssection.py
+++ nu_crosssection.py
@@ -294,8 +294,10 @@ class EvESCrossSectionNLO:
         z = (1 - beta) / (1 + beta)
         L = np.log((1 + beta) / (1 - beta))
 
-        plyl = np.vectorize(lambda x: (mp.polylog(2, x)))
-        term1 = (plyl(z) - (np.pi**2) / 6) / beta
+        plyl = np.vectorize(lambda x: (mp.polylog(2, x)).real)
+        plyl_of_z = (plyl(z)).astype(float)
+
+        term1 = (plyl_of_z - (np.pi**2) / 6) / beta
         term2 = -(2 / beta) * (beta - 0.5 * L) * np.log((2 * eps) / lam)
         term3 = (L / (2 * beta)) * (1 + np.log(np.sqrt(1 - beta**2) * (1 + beta) / (4 * beta**2)))
 
@@ -341,9 +343,14 @@ class EvESCrossSectionNLO:
         z3 = z2 * (1.0 + beta) / (1.0 - beta)                         # typically ≤ 0
 
         # Assemble terms
-        plyl = np.vectorize(lambda x: (mp.polylog(2, x)))
+        plyl = np.vectorize(lambda x: (mp.polylog(2, x)).real)
+
+        plyl1 = (plyl(z1)).astype(float)
+        plyl2 = (plyl(z2)).astype(float)
+        plyl3 = (plyl(z3)).astype(float)
+
         t_poly = (0.5 + np.log(A)) * Lbeta \
-                - plyl(z1) -  plyl(z2) +  plyl(z3) \
+                - plyl1 -  plyl2 +  plyl3 \
                 + (np.pi**2)/6.0
 
         out = (t_poly / beta) + np.log(B) - 1.0
@@ -356,16 +363,19 @@ class EvESCrossSectionNLO:
         rho  = np.sqrt(1 - beta**2)
         Lb   = np.log((1 + beta) / (1 - beta))
 
-        termA = (beta - 0.5 * Lb) * mp.log(M_E / lam)
+        termA = (beta - 0.5 * Lb) * np.log(M_E / lam)
         termB = ((3 + rho) / 8) * Lb
         termC = -(1/8) * Lb * np.log(2 * (1 + rho) / rho)
 
         arg1 = (beta - 1 + rho) / (2 * beta)
         arg2 = (beta + 1 - rho) / (2 * beta)
 
-        plyl = np.vectorize(lambda x: (mp.polylog(2, x)))
+        plyl = np.vectorize(lambda x: (mp.polylog(2, x)).real)
 
-        termD = -0.5 * (plyl(arg1) - plyl(arg2))
+        plyl_arg1 = (plyl(arg1)).astype(float)
+        plyl_arg2 = (plyl(arg2)).astype(float)
+
+        termD = -0.5 * (plyl_arg1 - plyl_arg2)
         f1 = (termA + termB + termC + termD) / beta - 1
 
         return 2 * f1
@@ -422,13 +432,141 @@ class EvESCrossSectionNLO:
                                                         + (2/3) * self.PiFF(q2, M_TOP, mu) * dsigma_up \
                                                         + (-1/3) * self.PiFF(q2, M_BOTTOM, mu) * dsigma_down \
                                                     )
-        # TODO(AT): add uds contribution
         dsigma_dyn_uds_reduced = self.dsigma_dEl_LO(El, Enu, self.cc_prefactor * self.cL_nul,
                                                     self.cc_prefactor * self.cR,
                                                     self.cc_prefactor * (self.cL_nul + self.cR))
         PiGammaGamma_2GeV = 3.597
         return dsigma_dyn_lep_heaavy_quark + (ALPHA/np.pi) * (PiGammaGamma_2GeV - 2*self.sw2 * PiGammaGamma_2GeV)*dsigma_dyn_uds_reduced
 
+    def I_nonfact(self, xyzrqv, Enu, El):
+        # Generic non-factorizable kinematic piece
+        # takes in tuple of xi, yi, zi, ri, qi, vi (xyzqrv)
+        xi, yi, zi, ri, qi, vi = xyzrqv
+        prefactor = np.pi**2 / Enu**3
+
+        beta = np.sqrt(1 - np.power(M_E/El, 2))
+        rho = np.sqrt(np.clip(1.0 - beta**2, 0.0, np.inf))
+        l0 = M_E + Enu - El
+
+        beta_doppler = (1+beta)/(1-beta)
+        beta_plus_one_over_rho = (1 + beta)/rho
+
+        log1_arg = (2*Enu/M_E) / (-1 + (1 + 2*Enu/M_E)/beta_plus_one_over_rho)
+        log2_arg = (2*l0/M_E) / (1 + 2*Enu/M_E - beta_plus_one_over_rho)
+        log3_arg = (1 - beta_plus_one_over_rho) / (beta_doppler - beta_plus_one_over_rho * (1 + 2*Enu/M_E))
+
+        plyl = np.vectorize(lambda x: (mp.polylog(2, x)).real)
+
+        plyl1 = (plyl(beta_plus_one_over_rho)).astype(float)
+        plyl2 = (plyl(1 + 2*Enu/M_E)).astype(float)
+        plyl3 = (plyl((1+2*Enu/M_E)/beta_plus_one_over_rho)).astype(float)
+
+        return prefactor * (zi + yi*np.log(log1_arg) + xi*np.log(log2_arg) + ri*log(log3_arg) \
+                            + vi*(plyl1 - plyl2 + plyl3 - np.pi**2 / 6) \
+                            + qi*np.log(beta_doppler))
+
+    def IL_nonfact(self, Enu, El):
+        # Non-facttorizable kinematic pieces        
+        beta = np.sqrt(1 - np.power(M_E/El, 2))
+        rho = np.sqrt(np.clip(1.0 - beta**2, 0.0, None))
+
+        vL = 0.5 * (M_E**2 / 2 + 2*M_E*Enu + Enu**2)
+        xL = (-2/15 * Enu**5/M_E**3 + 1/3 * Enu**3/M_E
+            + ( (1 + 3*beta**2)/(3*rho**3) - (4*beta**4 - 11*beta**2 + 7)/(3*rho**4) ) * Enu**2
+            + (2/rho**3 - (beta**4 - beta**2 + 2)/rho**4) * M_E*Enu
+            + ( (-7*beta**4 + 14*beta**2 - 22)/(15*rho**4) + (15*beta**4 - 25*beta**2 + 22)/(15*rho**5) ) * M_E**2 )
+        yL = 0.5 * Enu * (Enu - M_E)
+        rL = ( (-(2 + beta)/3 * rho/(1 + beta)**2 + (14 + beta)/6/(1 + beta))*Enu**2
+            + ( (beta - rho**2)/(rho*(1 + beta)) + 0.5*(1 + 1/(1 + beta)**2) )*M_E*Enu
+            + ( (-(17*beta**2 + 36*beta + 22))/(30*(1 + beta)**3)
+                + (14*beta**2 + 43*beta + 44)/(60*(1 + beta)**2) )*M_E**2 )
+        qL = ((1/2* rho/(1 + beta) - (1 + beta)/(2*beta))*Enu**2
+            + beta/(2*rho)*M_E*Enu
+            + (1/2* rho/(1 + beta) - (1 + beta)/(2*beta))*M_E**2)
+        
+        zL0 = (25*beta**2 - 49)/(60*rho**3) * (1 - 1/rho) - 8*beta**2/(15*rho**2)
+        zLw = (-20*beta**3 + 51*beta**2 + 38*beta - 105)/(60*rho**3) - (55*beta**3 + 54*beta**2 - 82*beta - 105)/(60*rho**2*(1 + beta))
+        zLw2 = (7*beta**2 + 8*beta - 23)/(30*(1 + beta)*rho) - (15*beta**2 + 6*beta - 23)/(30*rho**2)
+        zLw3 = (3 - beta)/(30*rho) - (3 + 2*beta)/(30*(1+beta))
+        zLw4 = 1/15 - rho/(15*(1 + beta))
+
+        zL = (zLw4*Enu**4 
+            + zLw3*Enu**3*M_E*Enu 
+            + zLw2*Enu**2*M_E**2 
+            + zLw*Enu*M_E**3*Enu 
+            + zL0*M_E**4)
+        
+        return self.I_nonfact(np.array([xL, yL, zL, rL, qL, vL]), Enu, El)
+
+    def IR_nonfact(self, Enu, El):
+        beta = np.sqrt(1 - np.power(M_E/El, 2))
+        rho = np.sqrt(np.clip(1.0 - beta**2, 0.0, None))
+        l0 = M_E + Enu - El
+
+        vR = 0.5 * (l0**2 + M_E**2 * (beta**2 + rho)/rho**2)
+        xR = -l0**2 * (35*l0*M_E**2 - 10*l0**2*M_E + 2*l0**3 - 30*M_E**3) / (15*M_E**3)
+        yR = (-Enu**4 
+            - 2*(5 - 1/rho)*M_E*Enu**3
+            + (128*beta**2 + 11*rho - 16)/rho**2 * M_E**2*Enu**2
+            + (6*beta**2 + 9*rho - 10)/rho**2 * M_E**3*Enu
+            + (beta**2 + 2*rho - 2)/rho**2 * M_E**4) / (M_E + 2*Enu)**2
+        qR = ((1/2* rho/(1 + beta) - (1 + beta)/(2*beta))*l0**2
+            + ((2 - 1/(1 + beta)) - (2 - beta)/(2*rho))*M_E*l0
+            + ((4*beta**3 + beta**2 - 4*beta + 2)/(4*beta*rho**2) 
+                + (-beta**3 + 2*beta**2 + beta - 1)/(2*beta*rho*(1 + beta)))*M_E**2)
+        rR = ( (-(2 + beta)/3 * rho/(1 + beta)**2 + (14 + beta)/6/(1 + beta))*l0**2
+            + ( (beta**2 - 5*beta + 1)/(3*rho*(1 + beta)) 
+                + (7*beta**2 + 8*beta - 2)/(6*(1 + beta)**2) )*M_E*l0
+            + ( (-23*beta**3 + 14*beta**2 + 41*beta - 2)/(30*rho*(1 + beta)**2) 
+                + (-28*beta*rho**2 + 43*beta**2 + 2)/(30*rho*(1 + beta)**2) )*M_E**2 )
+        
+        
+        zRw4 = -8/(15*rho) + (18 - beta)/(15*(1 + beta))
+        zRw3 = (113*beta**2 - 2*beta - 133)/(30*(1 + beta)*rho) - (143*beta**2 - 34*beta - 133)/(30*rho**2)
+        zRw2 = (-339*beta**3 - 805*beta**2 - 353*beta + 851)/(60*rho**3) + (-760*beta**3 - 825*beta**2 + 778*beta + 851)/(60*rho**2*(1 + beta))
+        zRw = (beta*((433 - 45*beta)*beta + 44) - 439)/(30*rho**3) + beta*(beta*(27*beta*(11*beta + 1) - 730) - 29) + 439/(30*rho**4)
+        zR0 = (270*beta**2 - 269)/(60*rho**3) + (309*beta**4 - 839*beta**2 + 538)/(120*rho**4)
+        zR = (2*Enu**4*Enu**5 
+            + zRw4*Enu**4*M_E**4 
+            + zRw3*Enu**3*M_E**3*Enu 
+            + zRw2*Enu**2*M_E**2*Enu**2 
+            + zRw*M_E**3*Enu**3*Enu**2 
+            + zR0*M_E**5*Enu) / (M_E**2*(M_E + 2*Enu))
+        
+        return self.I_nonfact(np.array([xR, yR, zR, rR, qR, vR]), Enu, El)
+
+    def ILR_nonfact(self, Enu, El):
+        beta = np.sqrt(1 - np.power(M_E/El, 2))
+        rho = np.sqrt(np.clip(1.0 - beta**2, 0.0, None))
+        l0 = M_E + Enu - El
+
+        vLR = 0.5 * M_E * (2*l0 - M_E)
+        xLR = (3*l0*M_E**2 - 3*l0**2*M_E - 2*l0**3 + 3*M_E**2*Enu) / (3*M_E)
+        yLR = M_E * El * (1 - ((M_E + 2*Enu)**2 - M_E*Enu)/(El*(M_E + 2*Enu)))
+        rLR = (1/3 * (7 + 5*beta/2 + (2*beta**2 - 4*beta - 7)/rho) * M_E**2/(1 + beta)
+            + (1 + (1 - 2*rho)/(1 + beta))*M_E*Enu)
+        qLR = ((1 - beta)/beta * Enu**2 
+            - 2*M_E*Enu 
+            + (1 + beta/2)/M_E * M_E**2 * (l0 - Enu/M_E) 
+            + beta*M_E*El)
+        zLR = (2*l0 + 9*M_E)/6 * (l0 - rho*Enu/(1 + beta))
+
+        return self.I_nonfact(np.array([xLR, yLR, zLR, rLR, qLR, vLR]), Enu, El)
+    
+    def dsigma_NF_total(self, El, Enu):
+        cL_sq_eff = self.cL_nul**2
+        cR_sq_eff = self.cR**2
+        cLcR_eff = self.cL_nul * self.cR
+        dsigma = 0.0
+        prefactor = (ALPHA/4/np.pi**4) * M_E * Enu
+        if self.cp == 1:
+            dsigma = prefactor * (cL_sq_eff*self.IL_nonfact(El, Enu) + cR_sq_eff * self.IR_nonfact(El, Enu) \
+                                    + cLcR_eff*self.ILR_nonfact(El, Enu))
+        elif self.cp == -1:
+            dsigma = prefactor * (cL_sq_eff * self.IR_nonfact(El, Enu) + cR_sq_eff*self.IL_nonfact(El, Enu) \
+                                    + cLcR_eff*self.ILR_nonfact(El, Enu))
+        
+        return dsigma
 
     def dsigma_dEl_LO(self, El, Enu, cL_sq_eff=None, cR_sq_eff=None, cLcR_eff=None):
         if cL_sq_eff is None:
@@ -468,9 +606,7 @@ class EvESCrossSectionNLO:
         
         dsigma_lo_corr = (1 + deltas)*self.dsigma_dEl_LO(El, Enu)
 
-        # TODO(AT): add non factorizable terms
-
-        return dsigma_lo_corr + self.dsigma_dyn(El, Enu, mu) # self.dsigma_NF(El, Enu, mu) +
+        return dsigma_lo_corr + self.dsigma_dyn(El, Enu, mu) + self.dsigma_NF_total(El, Enu)
 
 
 
